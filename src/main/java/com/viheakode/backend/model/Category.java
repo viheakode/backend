179216996d37a506bf58ed6ee377d4636123a56d
viheakode <viheakode787@gmail.com>
2026-02-08 13:51:07 +0700

    Add config db

---
+++ src/main/java/com/viheakode/backend/model/Category.java
@@ -0,0 +1,22 @@
+package com.viheakode.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tblCategory")
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    private String categoryName;
+    private String description;
+    private String status;
+    private String publisher;
+    private Date publishedDate;
+    private Date modifiedDate;
+}
